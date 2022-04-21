18666c748364f2f61498ca125ca291bdf4641ce5
Aman Mahour <99683300+amanmahour22@users.noreply.github.com>
2022-04-21 16:12:25 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,9 @@
+<script
+  src="https://kit.fontawesome.com/30890b4726.js"
+  crossorigin="anonymous"
+></script>;
+
+document.querySelector(".fa-instagram").click();
+document.querySelector(".fa-linkedin").click();
+document.querySelector(".fa-github").click();
+document.querySelector(".fa-twitter-square").click();
