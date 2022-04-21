4cd92a90d66656026573f85c2cbcc3bac86d5281
Aman Mahour <99683300+amanmahour22@users.noreply.github.com>
2022-04-21 16:12:45 +0530

    Update resume.js

--- resume.js
+++ resume.js
@@ -1,8 +1,3 @@
-<script
-  src="https://kit.fontawesome.com/30890b4726.js"
-  crossorigin="anonymous"
-></script>;
-
 document.querySelector(".fa-instagram").click();
 document.querySelector(".fa-linkedin").click();
 document.querySelector(".fa-github").click();
